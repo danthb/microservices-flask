ab58b215a5ff42ccb0d04725fa5675b54af91fd3
htenezaca <h.tenezaca@uniandes.edu.co>
2022-08-30 17:31:56 -0500

    Updated backend

---
+++ flaskr/app.py
@@ -0,0 +1,41 @@
+from flaskr import create_app
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from .models import db
+from .views import SongsView, SongView, LogInView, SignInView,  AlbumView, AlbumsUserView, SongsAlbumVIew
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+
+db.init_app(app)
+db.create_all()
+
+
+api = Api(app)
+api.add_resource(SongsView, '/songs')
+api.add_resource(SongView, '/song/<int:id>')
+api.add_resource(SignInView, '/signin')
+api.add_resource(LogInView, '/login')
+api.add_resource(AlbumsUserView, '/user/<int:id_user>/albums')
+api.add_resource(AlbumView, '/album/<int:id_album>')
+api.add_resource(SongsAlbumVIew, '/album/<int:id_album>/songs')
+
+jwt = JWTManager(app)   
+
+
+#TESTING
+""" with app.app_context():
+    album_schema = AlbumSchema()
+    s = Song(title='Taking to the moon', min=2, seg=40, artist='Bruno Mars')
+    s1 = Song(title='Harry Styles', min=2, seg=40, artist='Bruno Parts')
+    u = User(name='Daniel', password='1234' )
+    u1 = User(name='Lourdes', password='1234' )
+    a = Album(title='Back to home', year=1980, description='text', media=Medio.CD)
+    a1 = Album(title='Live', year=1981, description='text in the world', media=Medio.CD)
+    db.session.add(a)
+    db.session.add(s1)
+    a.songs.append(s1)
+    db.session.commit()
+    print([album_schema.dumps(album) for album in Album.query.all()]) """
---
+++ flaskr/views/views.py
@@ -0,0 +1,146 @@
+from os import access
+from flask_restful import Resource
+from ..models import db, Song, SongSchema, User, UserSchema, Album, AlbumSchema, Medio
+from flask import request
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
+
+song_schema = SongSchema()
+user_schema = UserSchema()
+album_schema = AlbumSchema()
+
+
+class SongsView(Resource):
+    def get(self):
+        return [song_schema.dump(song) for song in Song.query.all()]
+    
+    def post(self):
+        new_song = Song(
+                                title=request.json['title'], 
+                                min=request.json['min'], 
+                                seg=request.json['seg'], 
+                                artist=request.json['artist'])
+    
+        db.session.add(new_song)
+        db.session.commit()
+        return song_schema.dump(new_song)
+    
+    
+class SongView(Resource):
+    def get(self, id):
+        return song_schema.dump(Song.query.get_or_404(id))
+    
+    def put(self, id):
+        song = Song.query.get_or_404(id)
+        song.title = request.json.get('title', song.title)
+        song.min = request.json.get('min', song.min)
+        song.seg = request.json.get('seg', song.seg)
+        song.artist = request.json.get('artist', song.artist)
+        db.session.commit()
+        return song_schema.dump(song) 
+    
+    
+    def delete(self, id):
+        song = Song.query.get_or_404(id)
+        db.session.delete(song)
+        db.session.commit()
+        return 'Ok', 204
+    
+    
+class LogInView(Resource):
+    def post(self):
+            u_name = request.json["name"]
+            u_password = request.json["password"]
+            user = User.query.filter_by(name=u_name, password = u_password).all()
+            if user:
+                return {'message':'LogIn successful'}, 200
+            else:
+                return {'message':'Wrong credentials'}, 401
+
+
+class SignInView(Resource):
+    
+    def post(self):
+        new_user = User(name=request.json["name"], password=request.json["password"])
+        access_token = create_access_token(identity=new_user.name)
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message':'SignIn successful', 'access_token':access_token}, 200 
+
+    def put(self, id_user):
+        user = User.query.get_or_404(id_user)
+        user.password = request.json.get("password",user.password)
+        db.session.commit()
+        return user_schema.dump(user)
+
+    def delete(self, id_user):
+        user = User.query.get_or_404(id_user)
+        db.session.delete(user)
+        db.session.commit()
+        return '',204
+
+class AlbumsUserView(Resource):
+    
+    @jwt_required()
+    def post(self, id_user):
+        nuevo_album = Album(title=request.json["title"], year=request.json["year"], description=request.json["description"], medio=request.json["medio"])
+        user = User.query.get_or_404(id_user)
+        user.albumes.append(nuevo_album)
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return 'user has a album with name',409
+
+        return album_schema.dump(nuevo_album)
+    
+    
+    @jwt_required()
+    def get(self, id_user):
+        user = User.query.get_or_404(id_user)
+        return [album_schema.dump(al) for al in user.albums]
+
+class SongsAlbumVIew(Resource):
+
+    def post(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        
+        if "id_song" in request.json.keys():
+            
+            nueva_songs = Song.query.get(request.json["id_song"])
+            if nueva_songs is not None:
+                album.songses.append(nueva_songs)
+                db.session.commit()
+            else:
+                return 'Canción errónea',404
+        else: 
+            nueva_songs = Song(title=request.json["title"], minutos=request.json["minutos"], segundos=request.json["segundos"], interprete=request.json["interprete"])
+            album.songses.append(nueva_songs)
+        db.session.commit()
+        return song_schema.dump(nueva_songs)
+       
+    def get(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        return [song_schema.dump(ca) for ca in album.songs]
+
+class AlbumView(Resource):
+
+    def get(self, id_album):
+        return album_schema.dump(Album.query.get_or_404(id_album))
+
+    def put(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        album.title = request.json.get("title",album.title)
+        album.year = request.json.get("year", album.year)
+        album.description = request.json.get("description", album.description)
+        album.medio = request.json.get("medio", album.medio)
+        db.session.commit()
+        return album_schema.dump(album)
+
+    def delete(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        db.session.delete(album)
+        db.session.commit()
+        return '',204
+
